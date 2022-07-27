544b72af3dd33fd586c0a9c1169af535b3d63af4
Chelly321 <michelle.royer@icloud.com>
2022-07-26 21:15:45 -0400

    deployment

--- src/main/java/nl/novi/soulfullapplication/SoulfulWebsite.java
+++ src/main/java/nl/novi/soulfullapplication/SoulfulWebsite.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SoulfullYogaCourseApplication {
+public class SoulfulWebsite {
 
     public static void main(String[] args) {
-        SpringApplication.run(SoulfullYogaCourseApplication.class, args);
+        SpringApplication.run(SoulfulWebsite.class, args);
     }
 
 }
--- src/test/java/nl/novi/soulfullapplication/SoulfulWebsiteTests.java
+++ src/test/java/nl/novi/soulfullapplication/SoulfulWebsiteTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class SoulfullYogaCourseApplicationTests {
+class SoulfulWebsiteTests {
 
     @Test
     void contextLoads() {
